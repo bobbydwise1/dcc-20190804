efbe53ba93930e07827406f5faceb0dd44e9fc1b
Robert Lee <bobbydwise1@hotmail.com>
2019-08-04 20:34:19 -0700

    Create save point.

--- scripts.js
+++ scripts.js
@@ -17,6 +17,21 @@ For example, given the following board:
 and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum number of steps required to reach the end is 7, since we would need to go through (1, 2) because there is a wall everywhere else on the second row.
 */
 
+const board = [
+[0,0,0,0],
+[1,1,0,1],
+[0,0,0,0],
+[0,0,0,0]
+]
+
+let start = [3,0];
+let stop = [0,0];
+
+const howManySteps = (start, stop) {
+  let counter = 0;
+  //find the shortest path, regardless of walls or not
+  for (y = start[0]; y)
+}
 
 $(document).ready(function() {
   $('#output-section-1').text(1);
